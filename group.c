d4dbb5d1ee5c497a2036a01ee7d16df5beee6913
Andy Grimsley <andrewkattinegrimsley@gmail.com>
2019-02-06 12:25:03 -0700

    Third Update

--- group.c
+++ group.c
@@ -21,6 +21,23 @@ void Andrew(void) {
 }
 
 int main(void) {
-
+    printf("*************************\n");
+    printf("        GroupSix\n");
+    printf("*************************\n");
+    printf("Members: \n");
+    printf("         -Andrew Kattine-Grimsley\n");
+    printf("         -Elana Kahn\n");
+    printf("         -Clay Kellinger\n");
+    printf("         -Brooke Kern\n");
+    printf("         -Cyrus Kirkeby\n");
+    
+    Elana();
+    Cyrus();
+    Brooke();
+    Clay();
+    Andrew();
+    
+    
+    return 0;
 
 }
